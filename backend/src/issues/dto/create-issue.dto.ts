9780596b61ccf72c4b2cd95687ca45f5a8f94dce
Rishabh <rishabhsharma100902@gmail.com>
2025-12-21 21:50:29 +0530

    Applying class-validator

--- backend/src/issues/dto/create-issue.dto.ts
+++ backend/src/issues/dto/create-issue.dto.ts
@@ -1,6 +1,15 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateIssueDto {
+    @IsString()
     type: string;
+
+    @IsString()
     description: string;
+
+    @IsNumber()
     lat: number;
+
+    @IsNumber()
     lng: number;
 }
--- backend/src/issues/dto/update-issue.dto.ts
+++ backend/src/issues/dto/update-issue.dto.ts
@@ -1,7 +1,18 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class UpdateIssueDto{
+    @IsString()
     type?: string;
+
+    @IsString()
     description?: string;
+
+    @IsNumber()
     lat?: number;
+
+    @IsNumber()
     lng? :number;
+
+    @IsString()
     status? :string;
 }
